bd75bd7992a3316313e1e247d72aac5d6dc315d9
Kawser Ahmed Shovon <ahmedshovoncse13@gmail.com>
2021-09-02 12:14:00 +0600

    made some changes and loading spinner added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
+// searching book 
 const loadBook = () => {
     const input = document.getElementById("book-input")
     const searchText = input.value
     input.value = ""
+
+    // if input length is greater than zero then show result 
     if (searchText.length == 0) {
         alert("Please Give input")
     } else {
@@ -9,32 +12,63 @@ const loadBook = () => {
             .then(res => res.json())
             .then(data => showResult(data))
     }
-    document.getElementById("book-found").textContent = ''
+    // clearing fields 
+    document.getElementById("book-found").innerHTML = ''
     document.getElementById("book-container").innerHTML = ''
+    spinner("block")
 }
+// search button click handler 
 document.getElementById("book-search").addEventListener("click", loadBook)
-const showResult = (books) => {
-    console.log(books);
+
+// spinner function
+const spinner = displayStatus => {
+    document.getElementById("spinner").style.display = displayStatus
+}
+
+// showing searched books 
+const showResult = books => {
+
+    // if search result not found then return message 
     const bookFound = books.numFound
+    if (books.numFound <= 0) {
+        document.getElementById("book-found").innerHTML = '<h1 class="text-center text-danger">SORRY, NO RESULTS FOUND !!</h1>'
+    }
+
+    // attatching searched result here 
     const bookContainer = document.getElementById("book-container")
     books.docs.forEach(book => {
-        console.log(book);
-        document.getElementById("book-found").textContent = `Total Books Found: ${bookFound}`
+        document.getElementById("book-found").innerHTML = `<h3 class="py-2">Total Books Found: <span class="text-success fw-bold">${bookFound}</span></h3>`
         const div = document.createElement("div")
         const cover = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
-        div.classList.add("col", "d-md-flex", "align-items-center", "bg-light", "p-4", "p-md-2", "rounded", "border")
+        const coverNotFound = `../images/no-image.png`
+        div.classList.add("col")
+
+        // setting up searched books
         div.innerHTML = `
-                <div class="text-center me-md-3">
-                    <img src="${cover}" alt="Cover Image" class="rounded" width = 130px>
+            <div class="border h-100 d-md-flex align-items-center p-2">
+                <div class="text-center me-md-3 mb-md-0 mb-3 ">
+
+                    <img src="${book.cover_i ? cover : coverNotFound}" alt="Cover Image" class="rounded" width=130px>
                 </div>
                 <div>
                     <h4>${book.title}</h4>
-                    <h5 class="fw-light"><small class="fw-normal">by </small>${book.author_name}</h5>
-                    <p class="text-secondary mt- mb-0"><span class="text-dark">Publisher </span>${book.publisher}</p>
-                    <p class="text-secondary mt- mb-0"><span class="text-dark">First published in </span>${book.first_publish_year}</p>
-                    <p class="text-secondary"><span class="text-primary">${book.edition_count} editions</span></p>
-                </div>
+                    <h5 class="fw-light"><small class="fw-normal">by </small>${book.author_name ? book.author_name :
+                'Author Name Not Found'}</h5>
+            <p class="text-secondary mt- mb-0" > <span class="text-dark">Publisher </span>${book.publisher ?
+                book.publisher :
+                'Publisher Not Found'
+            }</p >
+                    <p class="text-secondary mt- mb-0"><span class="text-dark">First published in
+                        </span>${book.first_publish_year ?
+                book.first_publish_year : ''}${book.publish_place ? `, ${book.publish_place}` : ''}</p>
+                    <p class="text-secondary"><span class="text-primary">${book.edition_count} ${book.edition_count
+                > 1
+                ? "editions" :
+                "edition"}</span></p>
+                </div >
+            </div >
     `
         bookContainer.appendChild(div)
     });
+    spinner("none")
 }
